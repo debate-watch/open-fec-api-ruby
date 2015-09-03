613d04ed59302114775ac9438d17f9e964f8ecfb
Chris Condon <chris@Parnassus-2.local>
2015-09-03 12:44:06 -0500

    Add Committee endpoint

--- lib/open_fec_api/client.rb
+++ lib/open_fec_api/client.rb
@@ -47,5 +47,21 @@ module OpenFecApi
       response = self.class.get("/candidates", query: query)
       return Response.new(response)
     end
+
+
+
+    #Committee endpoint
+    #
+    #
+    def committees(options = {})
+      query = {'api_key' => @api_key}
+      request_params = ["page", "per_page", "year", "designation", "committee_type", "organization_type", "cycle", "party", "min_first_file_date", "candidate_id", "state", "committee_id", "name", "q", "max_first_file_date", "sort", "sort_hide_null", "sort_nulls_large"]
+      request_options = options.select{|k,v| request_params.include?(k.to_s)}
+      request_options.each do |k,v|
+        query.merge!({k.to_s => v})
+      end
+      response = self.class.get("/committees", query: query)
+      return Response.new(response)
+    end
   end
 end
